7a4862a16a0658654656edb2cca2d023ed813204
Matheus Miranda <matheusmirandabj@gmail.com>
2020-09-14 18:41:36 -0300

    Teste Modificado

--- work/aulajs/index.js
+++ work/aulajs/index.js
@@ -1,56 +1,54 @@
-
-
 const alunosTurmaA = [ //aqui fiz uma "tabela", com varios valores dentro
-  {
-    nome: `Matheus`,
-    nota: 9.2
-  },
-  {
-    nome: `Debora`,
-    nota: 5.6
-  },
-  {
-    nome: `Fedora`,
-    nota: 1.2
-  },
-  {
-    nome: `Fedora`,
-    nota: 8.2
-  },
+    {
+        nome: `Matheus`,
+        nota: 9.3
+    },
+    {
+        nome: `Debora`,
+        nota: 2.6
+    },
+    {
+        nome: `Fedora`,
+        nota: 1.4
+    },
+    {
+        nome: `Fedora`,
+        nota: 1.2
+    },
 
-  {
-    nome: `Fedora`,
-    nota: 6.2
-  }
+    {
+        nome: `Fedora`,
+        nota: 6.2
+    }
 
 ]
 
 const alunosTurmaB = [ //aqui fiz uma "tabela", com varios valores dentro
-  {
-    nome: `Joazinho`,
-    nota: 9.9
-  },
-  {
-    nome: `Firmino`,
-    nota: 5
-  },
-  {
-    nome: `Cleiton`,
-    nota: 7.2
-  },
-  {
-    nome: `Cleiton`,
-    nota: 7.5
-  }
+    {
+        nome: `Joazinho`,
+        nota: 9.9
+    },
+    {
+        nome: `Firmino`,
+        nota: 5
+    },
+    {
+        nome: `Cleiton`,
+        nota: 7.2
+    },
+    {
+        nome: `Cleiton`,
+        nota: 7.5
+    }
 ]
 
 function contmedia(alunos) {
-  let soma = 0
-  for (let i = 0; i < alunos.length; i++) {
-    soma = soma + alunos[i].nota
-  }
-  const media = soma / alunos.length
-  return media
+    let soma = 0
+    for (let i = 0; i < alunos.length; i++) {
+        soma = soma + alunos[i].nota
+    }
+    const media = soma / alunos.length
+    return media
 }
 
 const media1 = contmedia(alunosTurmaA)
@@ -61,12 +59,11 @@ const med = media1.toFixed(2)
 const med2 = media2.toFixed(2)
 
 function EnviaMsg(media, turma) {
-  if (media >= 6) { //fazer alguma coisa "SE for verdadeiro a condição"
-    console.log(`A media da ${turma} foi ${media}, Parabens turma, vocês passaram.`)
-  }
-  else { //faz outra coisa caso não for(colocar o que fazer abaixo)
-    console.log(`A media da ${turma} foi ${media}. Que pena, vocês nao passaram`)
-  }
+    if (media >= 6) { //fazer alguma coisa "SE for verdadeiro a condição"
+        console.log(`A media da ${turma} foi ${media}, Parabens turma, vocês passaram.`)
+    } else { //faz outra coisa caso não for(colocar o que fazer abaixo)
+        console.log(`A media da ${turma} foi ${media}. Que pena, vocês nao passaram`)
+    }
 }
 
 EnviaMsg(med, `Turma A`)
