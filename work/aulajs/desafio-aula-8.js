fdeadaad2cce41cf74597ba9e74c3cfa5195a848
Miranda <math@Mirandas-MacBook-Pro.local>
2020-12-13 13:24:22 -0300

    Desafio 6 rocketseat

--- work/aulajs/desafio-aula-8.js
+++ work/aulajs/desafio-aula-8.js
@@ -1,36 +1,36 @@
-//DEFINIÇÃO DAS VARIAVEIS
-const nome = `Milton Zumba`
-const peso = 90.6
-const alt = 1.75
+// //DEFINIÇÃO DAS VARIAVEIS
+// const nome = `Milton Zumba`
+// const peso = 90.6
+// const alt = 1.75
 
-//A CONTA QUE O APLICATIVO VAI FAZER PARA DAR O RESULTADO
-const imcconta = peso / (alt * alt)
+// //A CONTA QUE O APLICATIVO VAI FAZER PARA DAR O RESULTADO
+// const imcconta = peso / (alt * alt)
 
-//ESTOY TRANSFERINDO O VALOR DE DENTRO DO "IMCCONTA" PARA A AVRIAVEL IMC 
-// E DEFININDO QUE A QUANTIDADE DE CASA QUE UTILIZARA É (2) DUAS
-const imc = imcconta.toFixed(2)
+// //ESTOY TRANSFERINDO O VALOR DE DENTRO DO "IMCCONTA" PARA A AVRIAVEL IMC 
+// // E DEFININDO QUE A QUANTIDADE DE CASA QUE UTILIZARA É (2) DUAS
+// const imc = imcconta.toFixed(2)
 
-//DEFININDO CONDIÇÕES
-if (imc < 18.5) {
-  console.log(`${nome}, seu IMC é ${imc} e você está crakudo = Magro`)
-}
-else if (imc >= 18.5 && imc <= 24.9) {
-  console.log(`${nome}, seu IMC é ${imc}, seu peso é saudável`)
-}
-else if (imc >= 25 && imc <= 29.9) {
-  console.log(`${nome}, seu IMC é ${imc} e você está com sobrepeso`)
-}
-else {
-  console.log(`${nome}, seu IMC é ${imc} e você está Obesão nivel Wesley`)
-}
+// //DEFININDO CONDIÇÕES
+// if (imc < 18.5) {
+//   console.log(`${nome}, seu IMC é ${imc} e você está crakudo = Magro`)
+// }
+// else if (imc >= 18.5 && imc <= 24.9) {
+//   console.log(`${nome}, seu IMC é ${imc}, seu peso é saudável`)
+// }
+// else if (imc >= 25 && imc <= 29.9) {
+//   console.log(`${nome}, seu IMC é ${imc} e você está com sobrepeso`)
+// }
+// else {
+//   console.log(`${nome}, seu IMC é ${imc} e você está Obesão nivel Wesley`)
+// }
 //////////////////////////////////////////////////////////////////////////////
 
 // TEMPO DE CONTRIBUIÇÃO //
 
-const nome = "Marcos"
-const idade = 52
-const sexo = "M"
-const contrib = 43
+const nome = 'Debora'
+const idade = 55
+const sexo = "F"
+const contrib = 35
 
 const soma = idade + contrib
 
