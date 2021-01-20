0c94e8375e7bb2132f235c9a3d52d8d6de381f54
Miranda <math@Mirandas-MacBook-Pro.local>
2021-01-19 23:57:42 -0300

    teste final

---
+++ dio/teste.js
@@ -0,0 +1,19 @@
+nbbj jhk kllnjljbhvjbknb jb njh nn bj j khv vjcjfxfgxjccfxjgxf
+
+define([
+    'require',
+    'h
+    fg
+    h
+    fgh
+    g
+    h
+    fg
+    h
+    fg
+    hfg
+    '
+], function(require, factory) {
+    'use strict';
+    
+});
